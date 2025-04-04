8c42657bbac5ab31f05c44bcb4bae592f6f162fa
klap <klap50@gmail.com>
2025-04-04 12:23:40 -0300

    comit1

--- __manifest__.py
+++ __manifest__.py
@@ -1,12 +1,15 @@
 {
-    'name': 'Módulo RG 5329 - Percepción IVA 3%',
+    'name': 'RG 5329 Percepción IVA 3%',
     'version': '1.0',
+    'depends': ['sale', 'account', 'l10n_ar'],
     'author': 'FWCorp',
-    'depends': ['sale', 'product'],
+    'category': 'Accounting',
+    'description': 'Aplica automáticamente la percepción RG 5329 del 3% a productos alcanzados cuando el neto supera los $100.000 y el cliente es Responsable Inscripto.',
     'data': [
-        'data/product_data.xml',
-        'views/sale_order_view.xml',
+        'data/tax_data.xml',
+        'views/product_view.xml'
     ],
     'installable': True,
     'auto_install': False,
+    'application': False
 }
--- models/sale_order.py
+++ models/sale_order.py
@@ -1,36 +1,25 @@
 from odoo import models, api
-from odoo.exceptions import UserError
-
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
-    def _get_rg5329_product(self):
-        product = self.env.ref('modulo_rg5329.producto_rg_5329', raise_if_not_found=False)
-        if not product:
-            raise UserError("Debe existir un producto con el XML ID 'modulo_rg5329.producto_rg_5329'.")
-        return product
-
-    def aplicar_percepcion_rg5329(self):
+    @api.onchange("order_line", "partner_id")
+    def _onchange_rg_5329_check(self):
         for order in self:
-            if order.state not in ['draft', 'sent']:
-                continue
+            if order.partner_id.l10n_ar_afip_responsibility_type_id.code != "1":
+                return
 
-            total_afectado = sum(
+            total_alcanzado = sum(
                 line.price_subtotal
                 for line in order.order_line
-                if line.product_id.product_tmpl_id.x_studio_rg_5329_iva_3
+                if line.product_id.product_tmpl_id.x_rg_5329_iva_3
             )
 
-            ya_aplicado = order.order_line.filtered(
-                lambda l: l.product_id == self._get_rg5329_product()
-            )
-
-            if total_afectado > 100000 and not ya_aplicado:
-                order.order_line.create({
-                    'order_id': order.id,
-                    'product_id': self._get_rg5329_product().id,
-                    'product_uom_qty': 1,
-                    'price_unit': total_afectado * 0.03,
-                    'name': 'Percepción RG 5329 IVA 3%',
-                })
+            for line in order.order_line:
+                if (
+                    line.product_id.product_tmpl_id.x_rg_5329_iva_3
+                    and total_alcanzado > 100000
+                ):
+                    impuesto = self.env.ref("rg5329_percepcion.tax_rg_5329", raise_if_not_found=False)
+                    if impuesto and impuesto not in line.tax_id:
+                        line.tax_id += impuesto
