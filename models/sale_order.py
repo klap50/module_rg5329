e10eb1e8c741d13aac53d1bbebdb00152efed31a
klap <klap50@gmail.com>
2025-04-04 13:38:45 -0300

    test8

--- models/sale_order.py
+++ models/sale_order.py
@@ -3,7 +3,7 @@ from odoo import models, fields, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    @api.depends('order_line.price_total', 'order_line.product_id')
+    @api.depends('order_line.price_subtotal', 'order_line.product_id')
     def _compute_rg_5329_applicable(self):
         for order in self:
             total_rg_5329 = sum(
@@ -19,18 +19,16 @@ class SaleOrder(models.Model):
         store=True
     )
 
-    @api.onchange('order_line')
+    @api.onchange('order_line', 'order_line.product_id', 'order_line.price_unit')
     def _apply_rg_5329_tax(self):
+        rg_5329_tax = self.env['account.tax'].search([('name', '=', 'Percepción RG 5329 IVA 3%')], limit=1)
         for order in self:
-            if order.rg_5329_applicable:
-                rg_5329_tax = self.env['account.tax'].search([('name', '=', 'Percepción RG 5329 IVA 3%')], limit=1)
-                if rg_5329_tax:
-                    for line in order.order_line:
-                        if line.product_id.x_rg_5329_iva_3:
-                            line.tax_id |= rg_5329_tax
-            else:
-                rg_5329_tax = self.env['account.tax'].search([('name', '=', 'Percepción RG 5329 IVA 3%')], limit=1)
-                if rg_5329_tax:
-                    for line in order.order_line:
-                        if rg_5329_tax in line.tax_id:
-                            line.tax_id -= rg_5329_tax
+            for line in order.order_line:
+                # Remover el impuesto si ya está y no debe aplicarse
+                if rg_5329_tax in line.tax_id:
+                    line.tax_id -= rg_5329_tax
+
+            if order.rg_5329_applicable and rg_5329_tax:
+                for line in order.order_line:
+                    if line.product_id.x_rg_5329_iva_3:
+                        line.tax_id |= rg_5329_tax
